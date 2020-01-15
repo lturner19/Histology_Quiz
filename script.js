bc41dac4c0c887235338cdf6046ad2c564cce51a
Leandra Turner <calliet0715@gmail.com>
2020-01-15 06:18:01 -0600

    final score page and final score value now available

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ const startButton = document.getElementById("start-btn");
 const introText = document.getElementById("welcome");
 const questionEl = document.getElementById("question-container");
 const commentEl = document.getElementById("comment");
+const quizScore = document.getElementById("form");
+const userScore = document.getElementById("finalscore")
 let timeLeft = 75;
 let currentQuestion = 0;
-let score = "";
+let score = 0;
+
 //var scoreData = [];
 
-
-startButton.addEventListener("click", startQuiz);
+startButton.addEventListener("click", startQuiz); // initiates startQuiz function
 startButton.addEventListener("click", startTimer);
 
 
-
-//This allows the initial welcome info to "disappear" after the start button is
+//This allows the initial welcome info to "disappear" after the start button is clicked
 function startQuiz() {
     startButton.classList.add("hide")
     introText.classList.add("hide")
@@ -21,7 +22,7 @@ function startQuiz() {
     renderQuestions()
 };
 
-//This is for the timer to begin running after "start quiz" button is clicked
+//This intiates the timer to begin running after "start quiz" button is clicked
 function startTimer() {
     let quizTimer = setInterval(function () {
         document.getElementById("countdown").innerHTML = timeLeft;
@@ -29,16 +30,21 @@ function startTimer() {
         timeLeft.textContent = "" + timeLeft;
         //preventing the timer from going into negative numbers
         if (timeLeft <= 0) {
-            clearInterval(timeLeft);
+            clearInterval(quizTimer);
             document.getElementById("countdown").innerHTML = "Time is up";
+            showSummary();
         }
     }, 1000)
 }
 
-
 const renderQuestions = function () {
+    if (currentQuestion === quizArray.length) {
+        score = timeLeft;
+        timeLeft = 0;
+    }
     //This gets the questions from the array
     quizArray.forEach(function (question, indexQuestion) {
+
         var div = document.createElement("div");
 
         if (currentQuestion !== indexQuestion) {
@@ -66,37 +72,49 @@ const renderQuestions = function () {
     });
 }
 
+
 function nextQuestion() {
     var rightAnswer = this.getAttribute("data-answer")
     var choiceLi = this.textContent
 
+    //comparing the answer from quizArray with all the choices in the quizArray
     if (rightAnswer === choiceLi) {
         commentEl.textContent = "Correct";
+        currentQuestion++;
+        //allows the "correct" to show up for 1 second after user clicks correct choice
         setTimeout(() => {
             questionEl.innerHTML = "";
-            currentQuestion++;
             commentEl.innerHTML = "";
             renderQuestions();
         }, 1000);
     }
     else {
         commentEl.textContent = "Wrong";
+        //allows the "wrong" to show up for 1 second after user clicks incorrect answer
         setTimeout(() => {
             questionEl.innerHTML = "";
             currentQuestion++;
             commentEl.innerHTML = "";
+            //decrements timer 10 seconds
             timeLeft = timeLeft - 10;
             renderQuestions();
         }, 1000);
     }
 }
 
+function showSummary() {
+    console.log("showSummary");
+    var userMessage = "Your score is " + score
+    userScore.textContent = userMessage;
+    questionEl.classList.add("hide");
+    quizScore.classList.remove("hide")
+}
 
 
 
 
-
-/* will need to use this to store the user's score and initials when they are finished w/quiz
+/*
+will need to use this to store the user's time and initials when they are finished w/quiz
 function scoreData () {
     localStorage.setItem("scoreData", JSON.stringify (scoreData));
 
@@ -104,4 +122,4 @@ function scoreData () {
 
 
 remember to use JSON.stringify(); to make an object / value into a stringify  &
-JSON.parse(); to parse the string */
+JSON.parse(); to parse the string  */
